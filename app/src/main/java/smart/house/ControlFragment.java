a05b4012821d9fd725540fc736208425d25d23f6
michaelGonzalez1812 <michael.gonzalez1812@gmail.com>
2019-02-28 23:27:50 -0600

    Fragment info and control added

---
+++ app/src/main/java/smart/house/ControlFragment.java
@@ -0,0 +1,33 @@
+package smart.house;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ControlFragment extends Fragment {
+
+
+    public ControlFragment() {
+        // Required empty public constructor
+    }
+
+    public static ControlFragment newInstance(String param1, String param2) {
+        return new ControlFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.control_fragment, container, false);
+    }
+
+}
---
+++ app/src/main/java/smart/house/InfoFragment.java
@@ -0,0 +1,34 @@
+package smart.house;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InfoFragment extends Fragment {
+
+
+
+    public InfoFragment() {
+        // Required empty public constructor
+    }
+
+    public static InfoFragment newInstance() {
+        return new InfoFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.info_fragment, container, false);
+    }
+
+}
