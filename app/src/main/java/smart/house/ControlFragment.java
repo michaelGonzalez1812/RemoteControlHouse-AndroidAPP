7404ff2550afa77a4e61010315137b9bc51a5236
michaelGonzalez1812 <michael.gonzalez1812@gmail.com>
2019-03-12 22:38:25 -0600

    UI completa

--- app/src/main/java/smart/house/ControlFragment.java
+++ app/src/main/java/smart/house/ControlFragment.java
@@ -2,10 +2,13 @@ package smart.house;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatImageView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ControlFragment extends Fragment {
 
@@ -27,7 +30,12 @@ public class ControlFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.control_fragment, container, false);
+        View view = inflater.inflate(R.layout.control_fragment, container, false);
+
+        ((AppCompatImageView) view.findViewById(R.id.control_fragment__app_compat_imageview))
+                .setImageDrawable(getResources().getDrawable(R.drawable.croquis));
+
+        return view;
     }
 
 }
